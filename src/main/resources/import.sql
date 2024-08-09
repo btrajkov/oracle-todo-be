abf8fb06ff7de249ec80a7b1c88a94d837a8feda
Branislav Trajkov <branislav.trajkov@oracle.com>
2024-08-09 12:49:04 +0200

    Docker compose file added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 insert into Category(id, name) values (1, 'Work');
 insert into Category(id, name) values (2, 'Personal');
 insert into Category(id, name) values (3, 'Others');
-ALTER SEQUENCE category_seq RESTART WITH 4;
+ALTER TABLE category AUTO_INCREMENT = 4;
